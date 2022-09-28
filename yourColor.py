fc99358695f295c496b2ea9cc54e5d5b0f41b1af
Mansour <mnasor3000@outlook.sa>
2022-09-28 10:08:39 +0300

    first test

---
+++ yourColor.py
@@ -0,0 +1,32 @@
+print("First try")
+print ("   / | ")
+print ("  /  |")
+print (" /   |")
+print ("/_ _ |")
+color= input("enter your color")
+plural= input("inter plural noun: ")
+adjective= input(" enter an adjective: ")
+
+print("trees are "+color)
+print(plural+ " are mean")
+print("please keep it "+ adjective)
+
+
+
+
+num1= input("inter your first number: ")
+num2 = input("inter your secend number: ")
+result= int(num1)+int(num2)
+print(result)
+character_name = "Mansour"
+character_age = "22"
+print("  ")
+print(character_name.lower().islower())
+print(len(character_name))
+
+print("there was a boy called \"" + character_name+"\"")
+print("he was "+character_age +" years old")
+print("he liked his name " + character_name)
+print("but he wasn't happy about being "+character_age)
+
+print("pushhhh")
